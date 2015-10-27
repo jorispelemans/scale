f61391261a9c1a7673bdd85eed49fc4c342c69a9
jorispelemans <joris.pelemans@gmail.com>
2015-10-27 16:02:24 +0100

    more clean-up

--- baglm/models/languagemodel.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-class LanguageModel():
-
-	def __init__(self, file):
-		self.file = file
-
-	def calcProb(self, word, history):
-		raise NotImplementedError('must override calcProb()')
